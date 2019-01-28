8866b7bed6fbdfcf01c0aa3088e922eb12a43c69
Nayros <mauricioinformaticait25@gmail.com>
2019-01-28 16:00:17 +0100

    Basic Routes
    pendiente de mejora.

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 /**
  * Configurar las rutas para la aplicacion SportStore
  */
 Route::get('/', function () {
-    return view ('welcome');
+    return view('welcome');
 });
+
+Route::get('/category', function () {
+    return view('welcome');
+});
+Route::get('/products', function () {
+    return view('welcome');
+});
+
+Route::get('/products/{id}', function ($id) {
+    return view('welcome');
+})
+    ->where('id', '([0-9]+)');
