3172aefc9c8cb3cc40d4710a8554aa1bf082d60a
Nayros <34686122+Nayros@users.noreply.github.com>
2019-02-04 16:52:23 +0100

    Rutas cambiadas y actualizadas con los controladores de daniel 2
    todas las rutas para todos los controladores creados hasta la fecha, faltan 2 stock y images(Hablar como se haran estas dos ultimas)

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     //->where('username', '([a-zA-z0-9]+)');
 
 Route::get('/categories', function () {
-    return "Lista de categorias";
+    return "";
 });
 
 Route::get('/categories/products', function () {
@@ -77,31 +77,75 @@ Route::get('user/profile', 'UserProfileController@show');
 
 
 // Pasar nombre de la categoria y subcategoria (sql)
-Route::get('/categories/{categoryId}', function () {
-
-    return view('partials.subcategories');
-    //esto muestra todos las subcategoria de una categoria 
-    
-});
+Route::get('/categories/{categoryId}','CategoriesController@show');
 
 // Pasar nombre de la categoria, subcategoria (sql)
-Route::get('/categories/{categoryId}/sub_categories/{subCategoryId}', function () {
-    
-    return view('partials.products');
+Route::get('/categories/{categoryId}/sub_categories/{subCategoryId}','Sub_categoriesController@show'); 
     //Mostrara un producto completo apartir de su id viniendo desde la url
-});
-Route::get('/categories/{categoryId}/sub_categories/{subCategoryId}/product/{productId}', function () {
-    
-    return view('partials.product-detail');
+
+Route::get('/categories/{categoryId}/sub_categories/{subCategoryId}/product/{productId}','ProductsController@show');
     //Mostrara un producto completo apartir de su id viniendo desde la url
-});
+    
+
+//POR MODIFICAR:
+//-STOCK RUTAS Y IMAGES RUTAS (HABLAR ENTRE TODOS).
 
 
-// Pasar nombre de la categoria, subcategoria (sql)
-Route::get('/user/{userId}', function () {
-    return "hola";
-    //pagina que muestra el perfil con la id de ese usuario
-});
+//Rutas para las lineas de factura 
+Route::get('/user/{userId}/invoice_line/{invoiceLineId}/edit','Invoice_linesController@edit');
+Route::get('/user/{userId}/invoice_line/create','Invoice_linesController@create');
+Route::get('/user/{userId}/invoice_line/store','Invoice_linesController@store');
+Route::get('/user/{userId}/invoice_line/{invoiceLineId}/update','Invoice_linesController@update');
+Route::get('/user/{userId}/invoice_line/delete','Invoice_linesController@destroy');
+
+
+
+//rutas para las marcas 
+Route::get('/user/{userId}/brands','BrandsController@index');
+Route::get('/user/{userId}/brands/{brandId}/show','BrandsController@show');
+Route::get('/user/{userId}/brands/{brandId}/edit','BrandsController@edit');
+Route::get('/user/{userId}/brands/create','BrandsController@create');
+Route::get('/user/{userId}/brands/{brandId}/update','BrandsController@update');
+Route::get('/user/{userId}/brands/store','BrandsController@store');
+Route::get('/user/{userId}/brands/{brandId}/destroy','BrandsController@destroy');
+
+
+
+//Rutas para el address del usuario
+Route::get('/user/{userId}/address/{addressId}/edit','AddressesController@edit');
+Route::get('/user/{userId}/address/create','AddressesController@create');
+Route::get('/user/{userId}/address/store','AddressesController@store');
+Route::get('/user/{userId}/address/{addressId}/update','AddressesController@update');
+Route::get('/user/{userId}/address/delete','AddressesController@destroy');
+
+
+
+// Rutas para las facturas del usuario
+Route::get('/user/{userId}/invoices/{invoiceId}/show','InvoicesController@show');
+Route::get('/user/{userId}/invoices/{invoiceId}/edit','InvoicesController@edit');
+Route::get('/user/{userId}/invoices/create','InvoicesController@create');
+Route::get('/user/{userId}/invoices/store','InvoicesController@store');
+Route::get('/user/{userId}/invoices/{invoiceId}/update','InvoicesController@update');
+Route::get('/user/{userId}/invoices/delete','InvoicesController@destroy');
+
+//Rutas para los sports
+Route::get('/user/{userId}/sports','SportsController@index');
+Route::get('/user/{userId}/sports/{sportId}/show','SportsController@show');
+Route::get('/user/{userId}/sports/{sportId}/edit','SportsController@edit');
+Route::get('/user/{userId}/sports/create','SportsController@create');
+Route::get('/user/{userId}/sports/{sportId}/update','SportsController@update');
+Route::get('/user/{userId}/sports/store','SportsController@store');
+Route::get('/user/{userId}/sports/{sportId}/destroy','SportsController@destroy');
+
+//Rutas para las deliveries
+Route::get('/user/{userId}/deliveries/{deliveryId}/show','DeliveriesController@index');
+Route::get('/user/{userId}/deliveries/{deliveryId}/edit','DeliveriesController@edit');
+Route::get('/user/{userId}/deliveries/create','DeliveriesController@create');
+Route::get('/user/{userId}/deliveries/store','DeliveriesController@store');
+Route::get('/user/{userId}/deliveries/{deliveryId}/update','DeliveriesController@update');
+Route::get('/user/{userId}/deliveries/destroy','DeliveriesController@destroy');
+
+
 
 
 //Route::post('/category/{subcategory}','subcategoryController@postIndex'=>)
@@ -115,4 +159,7 @@ Route::get('/profile', function () {
 Route::get('/user/id/mycart', function () {
     return view('partials.cart');
 });
+
+//Rutas para los controladores de dani
+
 
