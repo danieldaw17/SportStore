db292dd35beea1ee8a2b421a5b1a7e7061400e9d
Nayros <mauricioinformaticait25@gmail.com>
2019-02-07 18:59:46 +0100

    implementacion auth by mauri
    authentication

--- app/User.php
+++ app/User.php
@@ -2,15 +2,29 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-	public function invoices() {
-		return $this->belongsTo('App\Invoice');
-	}
+    use Notifiable;
 
-	public function addresses() {
-		return $this->hasMany('App\Address');
-	}
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('nick', 20);
-			$table->string('password', 255);
-			$table->string('name', 25);
-			$table->string('lastName', 25);
-			$table->string('string', 9);
-			$table->string('role', 10);
-			$table->string('email', 255);
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('nick', 20);
+            $table->string('lastName', 25);
+            $table->string('image', 70)->nullable();
+            $table->string('role')->default('user');
+            $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
