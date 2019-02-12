39dc1d925d74d52bf8e166535ea13b80a4b8ba0a
Daniel_Alarcon17 <39710042+DanielAlarconCuesta@users.noreply.github.com>
2019-02-12 12:42:59 +0100

    updated sub category and category

--- resources/views/partials/admin/formCategory.blade.php
+++ resources/views/partials/admin/formCategory.blade.php
@@ -11,32 +11,99 @@ New categorie
 
 @section("content")
 <div id="formCategorie">
-  {{-- Cambiar en funcion si recibe datos o no --}}
-  <h1>New category</h1>
-  {{-- pasar action --}}
-  <form method="post" action="#" onsubmit="return validateFormCategory();">
-    {{-- Name --}}
-    <div class="form-group">
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+  @if (isset($category))
+    <h1>Edit category</h1>
+    {{-- pasar action --}}
+    <form method="post" action="{{ url('user/'.$userId.'/Categories/'.$category->id.'/edit') }}" onsubmit="return validateFormCategory();" enctype="multipart/form-data">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      {{-- Name --}}
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+          </div>
+          <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="{{$category->name}}">
         </div>
-        <input type="text" id="name" name="name" class="form-control" placeholder="Name">
+        <span id="errorNameCategory"></span>
       </div>
-      <span id="errorNameCategory"></span>
-    </div>
-    {{-- Taxes --}}
-    <div class="form-group">
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+      {{-- Image --}}
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+          </div>
+          <input type="file" id="imagePath" name="imagePath" class="form-control">
         </div>
-        <input type="text" id="taxes" name="taxes" class="form-control" placeholder="Taxes">
+        <span id="errorImageCategory"></span>
       </div>
-      <span id="errorTaxes"></span>
-    </div>
-    <input type="submit" name="sendCategory" value="Create" class="btn btn-default waves-effect waves-light">
-  </form>
+      {{-- Taxes --}}
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+          </div>
+          <input type="text" id="taxes" name="taxes" class="form-control" placeholder="Taxes" value="{{$category->taxes}}">
+        </div>
+        <span id="errorTaxes"></span>
+      </div>
+      <span id="errors">
+        @if (!empty($errors) && count($errors)>0)
+              <ul>
+                  @foreach ($errors as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+        @endif
+      </span>
+      <input type="submit" name="sendCategory" value="Modify" class="btn btn-default waves-effect waves-light">
+    </form>
+  @else
+    <h1>New category</h1>
+    <form method="post" action="{{ url('user/'.$userId.'/Categories/create') }}" onsubmit="return validateFormCategory();"  enctype="multipart/form-data">
+      {{ csrf_field() }}
+      {{-- Name --}}
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+          </div>
+          <input type="text" id="name" name="name" class="form-control" placeholder="Name">
+        </div>
+        <span id="errorNameCategory"></span>
+      </div>
+      {{-- Image --}}
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+          </div>
+          <input type="file" id="imagePath" name="imagePath" class="form-control">
+        </div>
+        <span id="errorImageCategory"></span>
+      </div>
+      {{-- Taxes --}}
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="list-group-item"><i class="fa fa-edit prefix grey-text fa-1g" aria-hidden="true"></i></span>
+          </div>
+          <input type="text" id="taxes" name="taxes" class="form-control" placeholder="Taxes">
+        </div>
+        <span id="errorTaxes"></span>
+      </div>
+      <span id="errors">
+        @if (!empty($errors) && count($errors)>0)
+              <ul>
+                  @foreach ($errors as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+        @endif
+      </span>
+      <input type="submit" name="sendCategory" value="Create" class="btn btn-default waves-effect waves-light">
+    </form>
+  @endif
 </div>
 @stop
 
