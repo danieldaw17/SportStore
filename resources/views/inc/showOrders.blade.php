541de958339aacf57e969152c1473aa2e4e6eec8
Daniel_Alarcon17 <39710042+DanielAlarconCuesta@users.noreply.github.com>
2019-02-17 16:24:31 +0100

    coding my orders

--- resources/views/inc/showOrders.blade.php
+++ resources/views/inc/showOrders.blade.php
@@ -4,24 +4,14 @@
 </div>
 	@foreach ($invoices as $invoice)
 		<div class="col-md-3 probootstrap-animate" data-animate-effect="fadeIn">
-			<a href="">
 				<div class="service hover_service text-center">
-					<div class="imgProduct">
-						<img src="" alt="" title="">
-					</div>
-					<div class="contentProduct">
-						@php
-						@endphp
-						<h3 class="titleProduct">{{$invoice->created_at}}</h3>
-						{{--<h3 class="titleProduct">{{$date->year}}-{{$date->month}}-{{$date->day}}</h3>--}}
-						<h5 class="titleProduct">{{$invoice->totalPrice}}</h5>
+					<h3 class="titleProduct">{{$invoice->created_at}}</h3>
+					{{--<h3 class="titleProduct">{{$date->year}}-{{$date->month}}-{{$date->day}}</h3>--}}
+					<h5 class="titleProduct">{{$invoice->totalPrice}} €</h5>
 
-						<div class="optionsED">
-							<a href=""><button type="button" class="btn btn-success">See details</button></a>
-						</div>
+					<div class="optionsED">
+						<a href="{{ url('/MyOrders/'.$invoice->invoiceId) }}"><button type="button" class="btn btn-success">See details</button></a>
 					</div>
 				</div>
-			</a>
 		</div>
 	@endforeach
-</div>
