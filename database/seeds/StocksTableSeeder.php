c936d1718b6e1f9d99e368ae477e625f89605dac
Nayros <34686122+Nayros@users.noreply.github.com>
2019-02-17 01:06:10 +0100

    Terminado footer cambios al ingles

--- database/seeds/StocksTableSeeder.php
+++ database/seeds/StocksTableSeeder.php
@@ -12,77 +12,329 @@ class StocksTableSeeder extends Seeder
     public function run()
     {
         
-        $c = new Stock();
-        $c->amount = 70;
-        $c->size = 'S';
-        $c->productId = 1;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 70;
+        $s->size = 'S';
+        $s->productId = 1;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 30;
-        $c->size = 'L';
-        $c->productId = 1;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 30;
+        $s->size = 'L';
+        $s->productId = 1;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 43;
-        $c->size = 'XL';
-        $c->productId = 1;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 43;
+        $s->size = 'XL';
+        $s->productId = 1;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount =123;
-        $c->size = 'XXL';
-        $c->productId = 1;
-        $c->save();
+        $s = new Stock();
+        $s->amount =123;
+        $s->size = 'XXL';
+        $s->productId = 1;
+        $s->save();
         
-         $c = new Stock();
-        $c->amount = 87;
-        $c->size = 'XS';
-        $c->productId = 1;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 87;
+        $s->size = 'XS';
+        $s->productId = 1;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 39;
-        $c->size = 'M';
-        $c->productId = 1;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 39;
+        $s->size = 'M';
+        $s->productId = 1;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 134;
-        $c->size = 'XL';
-        $c->productId = 2;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 134;
+        $s->size = 'XL';
+        $s->productId = 2;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 22;
-        $c->size = 'L';
-        $c->productId = 2;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 22;
+        $s->size = 'L';
+        $s->productId = 2;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 70;
-        $c->size = 'XXL';
-        $c->productId = 2;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 70;
+        $s->size = 'XXL';
+        $s->productId = 2;
+        $s->save();
 
-        $
-         $c = new Stock();
-        $c->amount = 66;
-        $c->size = 'S';
-        $c->productId = 2;
-        $c->save();
+        
+        $s = new Stock();
+        $s->amount = 66;
+        $s->size = 'S';
+        $s->productId = 2;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 87;
-        $c->size = 'M';
-        $c->productId = 2;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 87;
+        $s->size = 'M';
+        $s->productId = 2;
+        $s->save();
 
-         $c = new Stock();
-        $c->amount = 230;
-        $c->size = 'XS';
-        $c->productId = 2;
-        $c->save();
+        $s = new Stock();
+        $s->amount = 230;
+        $s->size = 'XS';
+        $s->productId = 2;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 2;
+        $s->size = 'XL';
+        $s->productId = L;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 10;
+        $s->size = 'L';
+        $s->productId = 3;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 50;
+        $s->size = 'S';
+        $s->productId = 3;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 20;
+        $s->size = 'XS';
+        $s->productId = 3;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 86;
+        $s->size = 'XXL';
+        $s->productId = 4;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 56;
+        $s->size = 'XL';
+        $s->productId = 4;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 26;
+        $s->size = 'L';
+        $s->productId = ;4
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 20;
+        $s->size = 'L';
+        $s->productId = 5;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 35;
+        $s->size = 'S';
+        $s->productId = 5;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 230;
+        $s->size = 'XS';
+        $s->productId = 5;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 20;
+        $s->size = 'L';
+        $s->productId = 6;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 18;
+        $s->size = 'S';
+        $s->productId = 6;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 120;
+        $s->size = 'S';
+        $s->productId = 7;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 50;
+        $s->size = 'XS';
+        $s->productId = 7;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 130;
+        $s->size = 'L';
+        $s->productId = 8;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 35;
+        $s->size = 'M';
+        $s->productId = 8;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 20;
+        $s->size = 'S';
+        $s->productId = 8;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 25;
+        $s->size = 'XL';
+        $s->productId = 9;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 130;
+        $s->size = 'L';
+        $s->productId = 9;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 20;
+        $s->size = 'M';
+        $s->productId = 9;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 130;
+        $s->size = 'S';
+        $s->productId = 9;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 4;
+        $s->size = 'L';
+        $s->productId = 10;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 18;
+        $s->size = 'M';
+        $s->productId = 10;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 30;
+        $s->size = 'S';
+        $s->productId = 10;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 40;
+        $s->size = 'L';
+        $s->productId = 11;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 30;
+        $s->size = 'M';
+        $s->productId = 11;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 30;
+        $s->size = 'L';
+        $s->productId = 12;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 25;
+        $s->size = 'M';
+        $s->productId = 12;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 23;
+        $s->size = 'S';
+        $s->productId = 12;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 23;
+        $s->size = 'S';
+        $s->productId = 12;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 29;
+        $s->size = 'XXS';
+        $s->productId = 13;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 23;
+        $s->size = 'XSS';
+        $s->productId = 14;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 28;
+        $s->size = 'XSS';
+        $s->productId = 15;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 50;
+        $s->size = 'L';
+        $s->productId = 16;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 80;
+        $s->size = 'M';
+        $s->productId = 16;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 70;
+        $s->size = 'L';
+        $s->productId = 17;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 80;
+        $s->size = 'M';
+        $s->productId = 17;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 23;
+        $s->size = 'M';
+        $s->productId = 17;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 50;
+        $s->size = 'L';
+        $s->productId = 20;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 30;
+        $s->size = 'XL';
+        $s->productId = 20;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 45;
+        $s->size = 'M';
+        $s->productId = 21;
+        $s->save();
+        
+        $s = new Stock();
+        $s->amount = 35;
+        $s->size = 'S';
+        $s->productId = 21;
+        $s->save();
     }
 }
